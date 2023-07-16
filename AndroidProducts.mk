d2d87f0eb5990e7727e82024c6303a7b0aa1e9b8
George Lucas Gomes <gomes.georgelucas@gmail.com>
2023-07-16 16:44:18 -0400

    feat: add product properties

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,3 +5,12 @@ COMMON_LUNCH_CHOICES := \
 # Arquivo de configuração principal do produto
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/devtitans_plantio.mk
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.devtitans.name=Plant.io
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.product.devtitans.version=1.0
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.devtitans.hardware=ModelB
