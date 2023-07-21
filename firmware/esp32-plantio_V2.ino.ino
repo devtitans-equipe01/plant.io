07ae7d028c4be69d83b42904bf9423206ac0131c
IsaiasSaraiva <129560729+IsaiasSaraiva@users.noreply.github.com>
2023-07-21 14:59:41 -0400

    Update esp32-plantio_V2.ino.ino

--- firmware/esp32-plantio_V2.ino.ino
+++ firmware/esp32-plantio_V2.ino.ino
@@ -2,51 +2,47 @@
 #include <DallasTemperature.h>
 #include <DHT.h>
 
+// GPIO Sensor LDR
+#define LDR 36 // ESP32 pin GIOP36 (ADC0)
 
-//Deinindo Pinagem do sensor DHT11
-#define DHT_SENSOR_PIN  14 // ESP32 pin GIOP21 connected to DHT11 sensor
+// GPIO Sensor DHT11
+#define DHT_SENSOR_PIN  14 // Pino do ESP32 GIOP21 conectado ao sensor DHT11
 #define DHT_SENSOR_TYPE DHT11
 DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
 
-// GPIO where the DS18B20 is connected to
+// GPIO Sensor Temperatura do Solo
 const int oneWireBus = 21;
+
+//GPIO Sensor Umidade do Solo
 const int SOIL_MOISTURE_SENSOR_PIN = 34;
 
-// Setup a oneWire instance to communicate with any OneWire devices
+// Configura uma instância OneWire para se comunicar com dispositivos OneWire
 OneWire oneWire(oneWireBus);
 
-// Pass our oneWire reference to Dallas Temperature sensor 
+// Referência oneWire para o sensor de temperatura Dallas
 DallasTemperature sensors(&oneWire);
 
 int pool_delay = 200;
-
-// unsigned long period = 5000;
-// unsigned long time_now = 0;
 
 void setup() {
   Serial.begin(921600);
   pinMode(SOIL_MOISTURE_SENSOR_PIN, INPUT);
 
-  Serial.println("DevTITANS Plant.io initialized.");
+  Serial.println("DevTITANS Plant.io inicializado.");
   sensors.begin();
+  dht_sensor.begin();
 }
 
 void loop() {
   String serialCommand;
 
-  // if(millis() >= time_now + period) {
-  //   time_now += period;
-  //   //Serial.println("analog" + String(analogRead(SOIL_MOISTURE_SENSOR_PIN)));
-  //   Serial.println("Soil Moisture(%): " + String(smGetValue(),2)+"%");
-  // }
-
   while (Serial.available() > 0) {
     char serialChar = Serial.read();
-    serialCommand += serialChar; 
+    serialCommand += serialChar;
 
     if (serialChar == '\n') {
-        processCommand(serialCommand);
-        serialCommand = "";
+      processCommand(serialCommand);
+      serialCommand = "";
     }
   }
 
@@ -56,39 +52,40 @@ void loop() {
 void processCommand(String command) {
   command.trim();
   command.toUpperCase();
-  //Umidade do Solo
+  // Umidade do Solo = SM
   if (command == "GET_SM") {
-    Serial.println("RES GET_SM " + String(smGetValue(),2));
-  }//Temperatura
+    Serial.println("RES GET_SM " + String(smGetValue(), 2));
+  } // Temperatura do Solo = ST
   else if (command == "GET_ST") {
     sensors.requestTemperatures();
     float temperatureC = sensors.getTempCByIndex(0);
     Serial.print("RES GET_ST ");
     Serial.println(temperatureC);
-    //Serial.println("ºC");
-  }//Umidade Ambiente
-  else if(command == "GET_AM"){
-    float humi  = dht_sensor.readHumidity();
-    Serial.print("RES GET_AM");
+  } // Umidade Ambiente = AM
+  else if (command == "GET_AM") {
+    float humi = dht_sensor.readHumidity();
+    Serial.print("RES GET_AM ");
     Serial.println(humi);
-  }//Temperatura Ambiente
-  else if(command == "GET_AT"){
+  } // Temperatura Ambiente = AT
+  else if (command == "GET_AT") {
     float tempC = dht_sensor.readTemperature();
-    Serial.print("RES GET_AT");
+    Serial.print("RES GET_AT ");
     Serial.println(tempC);
-  }
-  else if (command.startsWith("POLL_DELAY")) {
+  }// LDR = AL
+  else if(command == "GET_AL"){
+    int analogValue = analogRead(LDR);
+    Serial.print("RES GET_AL ");
+    Serial.println(analogValue);
+  } else if (command.startsWith("POLL_DELAY")) {
     int poll_value = command.substring(11).toInt();
 
     if (poll_value >= 100) {
-        pool_delay = poll_value;
-        Serial.printf("RES POLL_DELAY %d\n", poll_value);
+      pool_delay = poll_value;
+      Serial.printf("RES POLL_DELAY %d\n", poll_value);
+    } else {
+      Serial.printf("RES POLL_DELAY -1\n");
     }
-    else {
-        Serial.printf("RES POLL_DELAY -1\n");
-    }
-  }
-  else {
+  } else {
     Serial.println("ERR Unknown command.");
   }
 }
@@ -96,3 +93,5 @@ void processCommand(String command) {
 float smGetValue() {
   return (map(analogRead(SOIL_MOISTURE_SENSOR_PIN), 0, 4095, 10000, 0) / 100.0);
 }
+
+
