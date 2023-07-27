6ec49d16a3c5c43998150e987755e67d1e7207a3
Lauro Brizidio <laurobrizidio@hotmail.com>
2023-07-26 20:27:41 -0400

    Added Android App

---
+++ PlantioApp/app/src/main/java/br/edu/icomp/plantioapp/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package br.edu.icomp.plantioapp.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import br.edu.icomp.plantioapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ PlantioApp/app/src/main/java/br/edu/icomp/plantioapp/ui/SplashScreen.kt
@@ -0,0 +1,15 @@
+package br.edu.icomp.plantioapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import br.edu.icomp.plantioapp.R
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
