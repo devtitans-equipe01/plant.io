8ede8d4fc3a9a779e2e3a3b6760c907ce5cabcfa
George Lucas Gomes <gomes.georgelucas@gmail.com>
2023-07-31 19:18:13 -0400

    feat: working hal and client

--- devtitans_plantio.mk
+++ devtitans_plantio.mk
@@ -29,3 +29,6 @@ PRODUCT_PACKAGES += devtitans.plantio-service
 
 # Device Framework Matrix (Declara que o nosso produto Plantio precisa do serviço plantio)
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/devtitans/plantio/device_framework_matrix.xml
+
+PRODUCT_PACKAGES += plantio_service_client
+
